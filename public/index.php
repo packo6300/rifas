8e26abb74a9071c2f1103eac1a0bbdf49a8f6a3b
packo6300 <packo6300@gmail.com>
2018-10-17 13:40:59 -0600

    add config.ini for add adsense banners

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 require '../vendor/autoload.php';
 $smarty=new SmartyBC();
 $smarty->setCompileDir(__DIR__.'/temp/');
-echo $_SERVER['REQUEST_URI'];
+//echo $_SERVER['REQUEST_URI'];
 $s=array("carlos","chuy","paco","sergio","jorge");
 $smarty->assign("val",$s[rand(0,4)]);
 $smarty->display(__DIR__.'/../app/views/index.tpl');
