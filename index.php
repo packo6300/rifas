b1a314dd0628652241fb8dfc118f2be82f626b3d
packo6300 <packo6300@gmail.com>
2018-10-19 15:41:14 -0600

    modificacion a raiz

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 $smarty=new SmartyBC();
-$smarty->setCompileDir(__DIR__.'/../temp/');
+$smarty->setCompileDir(__DIR__.'/temp/');
 //echo $_SERVER['REQUEST_URI'];
 $s=array("sergio","jorge");
 $menu=array(
@@ -13,4 +13,4 @@ $smarty->assign("footer","Copyright &copy; Your Website 2018");
 $smarty->assign("title","Title");
 $smarty->assign("menu",$menu);
 $smarty->assign("val",$s[rand(0,1)]);
-$smarty->display(__DIR__.'/../app/views/index.tpl');
+$smarty->display(__DIR__.'/app/views/index.tpl');
